5c9ccf011aa4df3ad8de8f4740824d3483b552cf
Dany Marcoux <danymarcoux@gmail.com>
2018-10-14 22:15:51 +0200

    Align elements in maps

--- src/globals.ts
+++ src/globals.ts
@@ -9,7 +9,7 @@ export enum CreepRole {
 // TODO: Could the Map be a type?
 export const Roles: Map<CreepRole, number> = new Map<CreepRole, number>([
   [CreepRole.Harvester, 5],
-  [CreepRole.Upgrader, 5],
-  [CreepRole.Builder, 1],
-  [CreepRole.Repairer, 1]
+  [CreepRole.Upgrader,  5],
+  [CreepRole.Builder,   1],
+  [CreepRole.Repairer,  1]
 ]);
