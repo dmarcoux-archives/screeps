24c0ca9cb35075708edd3504fe71cac0f9bc297b
Dany Marcoux <danymarcoux@gmail.com>
2018-10-14 20:25:57 +0200

    Add TODO to spawn creeps on demand and just in time

--- src/spawner.ts
+++ src/spawner.ts
@@ -1,5 +1,7 @@
 import { CreepRole, Roles } from 'globals';
 
+// TODO: Spawn creep on demand just in time for a creep which will die (check creep.ticksToLive)
+
 // Spawn creeps
 export class Spawner {
   private room: string;
@@ -12,8 +14,8 @@ export class Spawner {
     for (const [role, targetNumber] of Roles) {
       // TODO: Is there a better way to compare the enums beside having to do `.valueOf()`
       const currentNumber: number = _.filter(Game.creeps, (creep) => creep.memory.role.valueOf() === role.valueOf()).length;
-
       console.log(`Role ${CreepRole[role]} - Current: ${currentNumber} - Target: ${targetNumber}`);
+
       if (currentNumber < targetNumber) {
         const creepBody: BodyPartConstant[] = [WORK, WORK, CARRY, MOVE];
         const creepName: string = `${CreepRole[role]}-${Game.time}`;
