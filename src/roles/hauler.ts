8de8f11f3a6508633f8e41dfa739e6ae66095af7
Dany Marcoux <danymarcoux@gmail.com>
2018-10-16 10:13:32 +0200

    Assign one hauler to each source

--- src/roles/hauler.ts
+++ src/roles/hauler.ts
@@ -9,6 +9,17 @@ export class Hauler {
   }
 
   public work() {
+    // TODO: Could this check be done only once, just after spawning?
+    if (!(this.self.memory.sourceId)) {
+      // TODO: This is limiting the number of haulers per source to 1. Multiple haulers per source might be better, if needed???
+      // alternative?: const countSourceIds: Dictionary<number> = _.countBy(_.filter(Memory.creeps, (memory) => memory.role === this.self.memory.role && memory.room === this.self.memory.room && memory.sourceId).map((memory) => memory.sourceId));
+      const busySourceIds: string[] = _.filter(Memory.creeps, (memory) => memory.role === this.self.memory.role && memory.room === this.self.memory.room && memory.sourceId).map((memory) => memory.sourceId);
+      // TODO: Could it be more efficient?
+      const freeSourceIds: string = Memory.rooms[this.self.memory.room].sourceIds.filter((value) => !busySourceIds.includes(value))[0];
+
+      this.self.memory.sourceId = freeSourceIds;
+    }
+
     if (this.self.memory.working) {
       // TODO: Don't hardcode the spawn
       switch(this.self.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY)) {
@@ -27,30 +38,28 @@ export class Hauler {
       }
     }
     else {
-      // TODO: Cache this in memory
-      // TODO: Assign creep to a source, so it doesn't go back and forth between sources
-      const energy: Resource<ResourceConstant> | null = this.self.pos.findClosestByPath(
-        FIND_DROPPED_RESOURCES,
-        {
-          filter: (resource) => resource.resourceType === RESOURCE_ENERGY
-        }
-      );
+      // TODO: Cache this in memory, perhaps by caching the location of the dropped energy???
+      const source: Source | null = Game.getObjectById(this.self.memory.sourceId);
 
-      if (energy) {
-        switch (this.self.pickup(energy)) {
-          case OK:
-            if (this.self.carry.energy === this.self.carryCapacity) {
+      if (source) {
+        const energy: Resource<ResourceConstant> | null = _.max(source.pos.findInRange(FIND_DROPPED_RESOURCES, 1, { filter: (resource) => resource.resourceType === RESOURCE_ENERGY }), (resource) => resource.amount);
+
+        if (energy) {
+          switch (this.self.pickup(energy)) {
+            case OK:
+              if (this.self.carry.energy === this.self.carryCapacity) {
+                this.self.memory.working = true;
+              }
+              break;
+            case ERR_FULL:
               this.self.memory.working = true;
-            }
-            break;
-          case ERR_FULL:
-            this.self.memory.working = true;
-            break;
-          case ERR_NOT_IN_RANGE:
-            if (this.self.moveTo(energy.pos) === OK) {
-              logMessage(`${this.self.name} => Energy`);
-            }
-            break;
+              break;
+            case ERR_NOT_IN_RANGE:
+              if (this.self.moveTo(energy.pos) === OK) {
+                logMessage(`${this.self.name} => Energy`);
+              }
+              break;
+          }
         }
       }
     }
