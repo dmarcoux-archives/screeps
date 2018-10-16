4e458747bcc21b849a2281542fb75816992dcd6f
Dany Marcoux <danymarcoux@gmail.com>
2018-10-16 23:23:37 +0200

    Reorder spawn order and add TODOs

--- src/globals.ts
+++ src/globals.ts
@@ -7,17 +7,17 @@ export enum CreepRole {
   Supplier = 'Supplier'
 }
 
-// TODO: Could the Map be a type?
+// TODO: Do not depend on elements in the map to determine the spawn order
+// TODO: Do not hardcode the number of creeps needed per role. Adapt it to the room and the needs
 export const Roles: Map<CreepRole, number> = new Map<CreepRole, number>([
   [CreepRole.Harvester, 2],
+  [CreepRole.Hauler,    2],
   [CreepRole.Upgrader,  2],
   [CreepRole.Builder,   2],
   [CreepRole.Repairer,  2],
-  [CreepRole.Hauler,    2],
-  [CreepRole.Supplier,  1],
+  [CreepRole.Supplier,  1]
 ]);
 
-// TODO: Could the Map be a type?
 export const RoleBodies: Map<CreepRole, BodyPartConstant[]> = new Map<CreepRole, BodyPartConstant[]>([
   [CreepRole.Harvester, [WORK, WORK, MOVE]],
   [CreepRole.Upgrader,  [WORK, WORK, CARRY, MOVE]],
