9c63eba2cfa59364afc0839950b057f659e998de
Dany Marcoux <danymarcoux@gmail.com>
2018-10-16 18:31:33 +0200

    Remove obvious TODOs

--- src/main.ts
+++ src/main.ts
@@ -11,16 +11,12 @@ import { ErrorMapper } from 'utils/ErrorMapper';
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
 export const loop = ErrorMapper.wrapLoop(() => {
-  // TODO: Store expansive operations (Game.creeps ???, etc...)
-
   // Automatically delete memory of missing creeps
   for (const name in Memory.creeps) {
     if (!(name in Game.creeps)) {
       delete Memory.creeps[name];
     }
   }
-
-  // TODO: Use types everywhere
 
   // TODO: Deal with multiple rooms
   let room: string = '';
