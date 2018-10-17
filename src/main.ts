f7102017aa7139f9fcb21fc1a29aad224a2afeec
Dany Marcoux <danymarcoux@gmail.com>
2018-10-17 21:12:16 +0200

    Use better variable name in loop

--- src/main.ts
+++ src/main.ts
@@ -12,9 +12,9 @@ import { ErrorMapper } from 'utils/ErrorMapper';
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
 export const loop = ErrorMapper.wrapLoop(() => {
   // Automatically delete memory of missing creeps
-  for (const name in Memory.creeps) {
-    if (!(name in Game.creeps)) {
-      delete Memory.creeps[name];
+  for (const creepName in Memory.creeps) {
+    if (!(creepName in Game.creeps)) {
+      delete Memory.creeps[creepName];
     }
   }
 
