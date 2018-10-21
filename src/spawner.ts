0048170037b019d3b1dbd3489851e78c025cb06e
Dany Marcoux <danymarcoux@gmail.com>
2018-10-21 14:30:19 +0200

    Use guard clause in spawner's code

--- src/spawner.ts
+++ src/spawner.ts
@@ -16,31 +16,33 @@ export class Spawner {
   public spawnCreeps() {
     const spawnQueue: Array<{ creepRole: CreepRole, memory: object }> = Memory.rooms[this.room].spawnQueue;
 
-    if (spawnQueue.length > 0) {
-      // TODO: Improve this. If spawn is busy, put (Game.time + wait) in memory to not run code for nothing
-      const spawning: Spawning | null = Game.spawns[this.spawn].spawning;
-      if (spawning) {
-        return;
-      }
+    if (spawnQueue.length === 0) {
+      return
+    }
 
-      const creepRole: CreepRole = spawnQueue[0].creepRole;
-      const creepBody: BodyPartConstant[] = RoleBodies.get(creepRole)!;
-      const creepName: string = `${creepRole}-${Game.time}`;
-      // Add role specific memory to the standard memory
-      const creepMemory: object = { memory: Object.assign(spawnQueue[0].memory, { room: this.room, role: creepRole, working: false }) };
-      // Add creep memory to the spawn options (which are empty for now...)
-      const spawnOptions: object = Object.assign(creepMemory, {});
+    // TODO: Improve this. If spawn is busy, put (Game.time + wait) in memory to not run code for nothing
+    const spawning: Spawning | null = Game.spawns[this.spawn].spawning;
+    if (spawning) {
+      return;
+    }
 
-      switch (Game.spawns[this.spawn].spawnCreep(creepBody, creepName, spawnOptions)) {
-        case OK:
-          logMessage(`${this.room} spawning ${creepRole}`);
-          // The creep is spawning, so we can remove it from the beginning of the queue
-          spawnQueue.shift();
-          break;
-        case ERR_NOT_ENOUGH_ENERGY:
-          logMessage(`${this.room} not enough energy to spawn ${creepRole}`);
-          break;
-      }
+    const creepRole: CreepRole = spawnQueue[0].creepRole;
+    const creepBody: BodyPartConstant[] = RoleBodies.get(creepRole)!;
+    const creepName: string = `${creepRole}-${Game.time}`;
+    // Add role specific memory to the standard memory
+    const creepMemory: object = { memory: Object.assign(spawnQueue[0].memory, { room: this.room, role: creepRole, working: false }) };
+    // Add creep memory to the spawn options (which are empty for now...)
+    const spawnOptions: object = Object.assign(creepMemory, {});
+
+    switch (Game.spawns[this.spawn].spawnCreep(creepBody, creepName, spawnOptions)) {
+      case OK:
+        logMessage(`${this.room} spawning ${creepRole}`);
+        // The creep is spawning, so we can remove it from the beginning of the queue
+        spawnQueue.shift();
+        break;
+      case ERR_NOT_ENOUGH_ENERGY:
+        logMessage(`${this.room} not enough energy to spawn ${creepRole}`);
+        break;
     }
   }
 }
