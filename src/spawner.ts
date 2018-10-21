f3bf412efba4760b7afda737c9044eb844864437
Dany Marcoux <danymarcoux@gmail.com>
2018-10-21 14:44:30 +0200

    Remove old TODO

--- src/spawner.ts
+++ src/spawner.ts
@@ -11,8 +11,6 @@ export class Spawner {
   }
 
   // TODO: Spawn creep on demand just in time for a creep which will die (check creep.ticksToLive)
-  // TODO: Add possibility to execute initialization code for a creep before it's spawned.
-  //       For example, when spawning an harvester, check if a container is next to the source he will harvest. If not, place construction site
   public spawnCreeps() {
     const spawnQueue: Array<{ creepRole: CreepRole, memory: object }> = Memory.rooms[this.room].spawnQueue;
 
