8d420ae9582b4d2571bce396f8cff14d89948672
Dany Marcoux <danymarcoux@gmail.com>
2018-10-21 14:07:33 +0200

    Use guard clauses for repairer's code

--- src/roles/repairer.ts
+++ src/roles/repairer.ts
@@ -10,58 +10,39 @@ export class Repairer extends Creep {
     if (this.memory.working) {
       const damagedStructureIds: string[] = Memory.rooms[this.memory.room].damagedStructureIds;
 
-      if (damagedStructureIds.length > 0) {
-        const damagedStructure: Structure | null = Game.getObjectById(damagedStructureIds[0]);
-        if (damagedStructure) {
-          switch(this.repair(damagedStructure)) {
-            case ERR_NOT_ENOUGH_RESOURCES:
-              this.memory.working = false;
-              break;
-            case ERR_NOT_IN_RANGE:
-              if (this.moveTo(damagedStructure.pos) === OK) {
-                logMessage(`${this.name} => Damaged Structure`);
-              }
-              break;
-          }
-        }
-        else {
-          // TODO: DRY this (duplicated code from builder)
-          // Act as a builder if there is nothing to repair
-          const constructionSite: ConstructionSite | null = this.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
-
-          if (constructionSite) {
-            switch(this.build(constructionSite)) {
-              case ERR_NOT_ENOUGH_RESOURCES:
-                this.memory.working = false;
-                break;
-              case ERR_NOT_IN_RANGE:
-                if (this.moveTo(constructionSite.pos) === OK) {
-                  logMessage(`${this.name} => Construction Site`);
-                }
-                break;
-            }
-          }
-        }
+      if (damagedStructureIds.length === 0) {
+        return;
       }
-    }
-    else {
-      // TODO: Cache this in memory
-      // TODO: Instead of RoomPosition.findInRange (medium CPU cost), use Room.lookForAtArea (low CPU cost)
-      const energy: Resource<ResourceConstant> | null = _.max(Game.spawns.Spawn1.pos.findInRange(FIND_DROPPED_RESOURCES, 1, { filter: (resource) => resource.resourceType === RESOURCE_ENERGY }), (resource) => resource.amount);
 
-      if (energy) {
-        switch (this.pickup(energy)) {
-            // case OK:
-            //   break;
-          case ERR_FULL:
-            this.memory.working = true;
-            break;
-          case ERR_NOT_IN_RANGE:
-            if (this.moveTo(energy.pos) === OK) {
-              logMessage(`${this.name} => Energy`);
-            }
-            break;
-        }
+      const damagedStructure: Structure = Game.getObjectById<Structure>(damagedStructureIds[0])!;
+      switch(this.repair(damagedStructure)) {
+        case ERR_NOT_ENOUGH_RESOURCES:
+          this.memory.working = false;
+          break;
+        case ERR_NOT_IN_RANGE:
+          if (this.moveTo(damagedStructure.pos) === OK) {
+            logMessage(`${this.name} => Damaged Structure`);
+          }
+          break;
+      }
+
+      return;
+    }
+
+    // TODO: Cache this in memory
+    // TODO: Instead of RoomPosition.findInRange (medium CPU cost), use Room.lookForAtArea (low CPU cost)
+    const energy: Resource<ResourceConstant> | null = _.max(Game.spawns.Spawn1.pos.findInRange(FIND_DROPPED_RESOURCES, 1, { filter: (resource) => resource.resourceType === RESOURCE_ENERGY }), (resource) => resource.amount);
+
+    if (energy) {
+      switch (this.pickup(energy)) {
+        case ERR_FULL:
+          this.memory.working = true;
+          break;
+        case ERR_NOT_IN_RANGE:
+          if (this.moveTo(energy.pos) === OK) {
+            logMessage(`${this.name} => Energy`);
+          }
+          break;
       }
     }
   }
