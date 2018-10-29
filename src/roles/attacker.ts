a090a917b99e4989b68dca8d076db618bafb4a3c
Dany Marcoux <danymarcoux@gmail.com>
2018-10-29 16:10:02 +0100

    Add pseudo code for attacker role

--- src/roles/attacker.ts
+++ src/roles/attacker.ts
@@ -8,5 +8,16 @@ export class Attacker extends Creep {
 
   public work() {
     // TODO: Code this
+    // Check if there is an attack flag placed.
+    //   If so, get in the room where it is
+    //     If already there, check if there is a target at the flag's position
+    //       If target, try to attack it
+    //         If OK, keep doing so
+    //         If not in range, move to flag position
+    //       If no target, look for enemy structures, creeps (by range, for the important structures like spawns or for other soldiers?)
+    //         If found, try to attack target
+    //           If OK, keep doing so
+    //           If not in range, move to target
+    //     If not already there, move to room
   }
 }
