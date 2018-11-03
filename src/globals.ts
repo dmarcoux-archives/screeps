883cae288706e78491b99187ff31abe0ba614b18
Dany Marcoux <danymarcoux@gmail.com>
2018-11-03 21:26:01 +0100

    Add extra parts to the remote builder role

--- src/globals.ts
+++ src/globals.ts
@@ -61,7 +61,7 @@ export const RoleBodies: Map<CreepRole, CreepBody> = new Map<CreepRole, CreepBod
   [CreepRole.Decoy,          { core: [TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, MOVE, MOVE], extra: [TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, MOVE, MOVE], maxExtra: 1 }],
   [CreepRole.Claimer,        { core: [MOVE, CLAIM], extra: [], maxExtra: 0 }],
   [CreepRole.Defender,       { core: [TOUGH, TOUGH, MOVE, MOVE, ATTACK], extra: [MOVE, ATTACK], maxExtra: 1 }],
-  [CreepRole.RemoteBuilder,  { core: [WORK, CARRY, MOVE, WORK, CARRY, MOVE], extra: [], maxExtra: 0 }]
+  [CreepRole.RemoteBuilder,  { core: [WORK, CARRY, MOVE, WORK, CARRY, MOVE], extra: [WORK, CARRY, MOVE], maxExtra: 1 }]
 ]);
 
 
