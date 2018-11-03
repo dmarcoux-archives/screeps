3e53941275ed82c0513a928ed1b5f3b4513b9495
Dany Marcoux <danymarcoux@gmail.com>
2018-11-03 21:49:17 +0100

    Improve code in ConstructionAutomater.setupSources
    It also ignores creeps now, so it doesn't find another path to sources
    if creeps are in the way

--- src/construction_automater.ts
+++ src/construction_automater.ts
@@ -1,5 +1,5 @@
 // TODO: Construct roads to minerals and their extractor
-// TODO: Construct roads between sources and room controller
+// TODO: Construct roads between drop point / storage and room controller
 // TODO: Construct walls/ramparts to block room entrances/exits and around controller
 // TODO: Construct towers (one next to spawn, others...???)
 // TODO: Construct extensions (next to a road close to the spawn. in a specific pattern?)
@@ -20,33 +20,28 @@ export class ConstructionAutomater {
       const sources: Source[] = this.room.find(FIND_SOURCES)
 
       for (const source of sources) {
-        // TODO: Improve this... just too many ifs
-        const builtContainer: StructureContainer = source.pos.findInRange<StructureContainer>(FIND_STRUCTURES, 1, { filter: (structure) => structure.structureType === STRUCTURE_CONTAINER })[0];
         let pathSteps: PathStep[];
-        if (builtContainer) {
-          pathSteps = position.findPathTo(builtContainer.pos);
+
+        const container: Structure | undefined = (source.pos.findInRange<StructureContainer>(FIND_STRUCTURES, 1, { filter: (structure) => structure.structureType === STRUCTURE_CONTAINER })[0] ||
+                                                  source.pos.findInRange(FIND_MY_CONSTRUCTION_SITES, 1, { filter: (c) => c.structureType === STRUCTURE_CONTAINER })[0]);
+
+        if (container) {
+          pathSteps = position.findPathTo(container.pos, { ignoreCreeps: true });
           pathSteps.pop(); // The last path step is the container, nothing to construct there
-          this.room.memory.sources.push({ id: source.id, containerPositionX: builtContainer.pos.x, containerPositionY: builtContainer.pos.y, pathLengthToFromDrop: pathSteps.length });
-        }
-        else {
-          const container: ConstructionSite = source.pos.findInRange(FIND_MY_CONSTRUCTION_SITES, 1, { filter: (c) => c.structureType === STRUCTURE_CONTAINER })[0];
-          if (container) {
-            pathSteps = position.findPathTo(container.pos);
-            pathSteps.pop(); // The last path step is the container, nothing to construct there
-            this.room.memory.sources.push({ id: source.id, containerPositionX: container.pos.x, containerPositionY: container.pos.y, pathLengthToFromDrop: pathSteps.length });
-          }
-          else {
-            pathSteps = position.findPathTo(source.pos);
+          this.room.memory.sources.push({ id: source.id, containerPositionX: container.pos.x, containerPositionY: container.pos.y, pathLengthToFromDrop: pathSteps.length });
 
-            // Construct container
-            pathSteps.pop(); // The last path step is the source, nothing to construct there
-            const containerPathStep: PathStep = pathSteps.pop()!;
-
-            this.room.memory.sources.push({ id: source.id, containerPositionX: containerPathStep.x, containerPositionY: containerPathStep.y, pathLengthToFromDrop: pathSteps.length })
-            const containerPosition: RoomPosition = new RoomPosition(containerPathStep.x, containerPathStep.y, this.room.name);
-            this.room.createConstructionSite(containerPosition, STRUCTURE_CONTAINER);
-          }
+          continue;
         }
+
+        // Construct container
+        pathSteps = position.findPathTo(source.pos, { ignoreCreeps: true });
+
+        pathSteps.pop(); // The last path step is the source, nothing to construct there
+        const containerPathStep: PathStep = pathSteps.pop()!;
+
+        this.room.memory.sources.push({ id: source.id, containerPositionX: containerPathStep.x, containerPositionY: containerPathStep.y, pathLengthToFromDrop: pathSteps.length })
+        const containerPosition: RoomPosition = new RoomPosition(containerPathStep.x, containerPathStep.y, this.room.name);
+        this.room.createConstructionSite(containerPosition, STRUCTURE_CONTAINER);
 
         // Construct roads
         for (const roadPathStep of pathSteps) {
